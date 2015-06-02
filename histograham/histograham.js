e59ee406eb664bcc5c76ca70c5c23bf3f66300fa
Chris Craig <codeman655@gmail.com>
2015-06-01 20:20:32 -0700

    not working, can't get chart on canvas

--- histograham/histograham.js
+++ histograham/histograham.js
@@ -14,7 +14,33 @@ if (Meteor.isClient) {
   Template.chart.helpers({
     generateChart: function() {
       console.log("making a chart");
-      return null;
+      //var ctx = $("#histogram").get(0).getContext("2d");
+      var ctx = document.getElementByID('histogram').getContext('2d');
+
+
+      var data = {
+        labels:["Execute", "Collaborate", "Candid", "Forward"],
+        datasets:[
+        {
+          label: "dataset_one",
+          fillColor: "rgba(220,220,220,0.5)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(220,220,220,0.75)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [4,3,12,10]
+        },
+        {
+          label: "dataset_two",
+          fillColor: "rgba(151,187,205,0.5)",
+          strokeColor: "rgba(151,187,205,0.8)",
+          highlightFill: "rgba(151,187,205,0.75)",
+          highlightStroke: "rgba(151,187,205,1)",
+          data: [3,1,10,5]
+        }
+        ]
+      }
+      var myBarChart = new Chart(ctx).Bar(data,options);
+      return mybarChart;
     }
   });
 
